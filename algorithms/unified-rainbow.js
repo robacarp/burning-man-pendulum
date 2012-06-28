978c4e473518dc893819cac074b91952d68ec1fb
Robert Carpenter <robacarp@gmail.com>
2012-06-28 20:56:34 +0300

    simple algorithm.

---
+++ algorithms/unified-rainbow.js
@@ -0,0 +1,15 @@
+function unified_rainbow(ball, position, tick){
+  //about 3300 ticks / cycle
+
+  //make each ball in the series different
+  //tick += 50 * position;
+
+  //just make the hue a function of time
+  hue = (tick % 155) / 155;
+
+  //doit
+  ball.color.hsl( hue , 1, 0.5 );
+}
+
+if (window.$algorithms)
+  $algorithms.push({name: 'huego', callback: unified_rainbow})
